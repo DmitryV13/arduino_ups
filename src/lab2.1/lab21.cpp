8e4d667da5b2e06b83429d97cf19711fc9010cd3
DmitryV13 <voiku173dmitry@gmail.com>
2025-10-07 00:41:46 +0300

    second lab 1

---
+++ src/lab2.1/lab21.cpp
@@ -0,0 +1,172 @@
+#include <Arduino.h>
+#include <LiquidCrystal.h>
+#include <stdio.h>
+
+#define BUTTON_TASK1 9
+#define BUTTON_INC 10
+#define BUTTON_DEC 11
+
+#define LED_DEPENDENT 3
+#define LED_BLINKING 2
+
+
+#define TIME_SEC 1000
+#define SYSTEM_TICK 1
+
+#define TASK_1_OFFSET (TIME_SEC * 1) / SYSTEM_TICK
+#define TASK_1_RECCURENCY (TIME_SEC / 20) / SYSTEM_TICK
+int task1Counter = TASK_1_OFFSET;
+
+#define TASK_2_OFFSET (TIME_SEC * 2) / SYSTEM_TICK
+int task2Reccurency = (TIME_SEC / 2) / SYSTEM_TICK;
+const byte additionalReccurency = 100;
+int task2Counter = TASK_2_OFFSET;
+
+#define TASK_3_OFFSET (TIME_SEC * 2) / SYSTEM_TICK
+#define TASK_3_RECCURENCY (TIME_SEC / 20) / SYSTEM_TICK
+int task3Counter = TASK_3_OFFSET;
+
+unsigned long lastTick = 0;
+
+
+LiquidCrystal lcd(13, 12, 7, 6, 5, 4);
+
+
+
+byte redLedState = LOW;
+byte greenLedState = LOW;
+
+byte previousButtonState = HIGH;
+byte currentButtonState = HIGH;
+
+boolean systemHasChanges = true;
+
+void setup() {
+  StdioSerialInit();
+  lcd.begin(16, 2);
+
+  pinMode(BUTTON_TASK1, INPUT_PULLUP);
+  pinMode(BUTTON_INC, INPUT_PULLUP);
+  pinMode(BUTTON_DEC, INPUT_PULLUP);
+
+  pinMode(LED_DEPENDENT, OUTPUT);
+  pinMode(LED_BLINKING, OUTPUT);
+
+}
+
+void loop() {
+  if (millis() - lastTick >= 1) {
+
+    if(--task1Counter <= 0){
+      taskLedPushButton();
+      task1Counter = TASK_1_RECCURENCY;
+    }
+  
+    if(--task2Counter <= 0){
+      taskLedBlink();
+      task2Counter = task2Reccurency;
+    }
+
+    if(--task3Counter <= 0){
+      taskIncDecPushButton();
+      task3Counter = TASK_3_RECCURENCY;
+    }
+
+    taskIdle();
+
+    lastTick = millis();
+  }
+}
+
+void taskLedPushButton() {
+    previousButtonState = currentButtonState;
+    currentButtonState = digitalRead(BUTTON_TASK1);
+    if (currentButtonState == LOW && currentButtonState != previousButtonState) {
+      redLedState = !redLedState;
+      digitalWrite(LED_DEPENDENT, redLedState);
+
+      systemHasChanges = true;
+    }
+}
+
+void taskIncDecPushButton() {
+    if(redLedState == HIGH){
+      if (digitalRead(BUTTON_INC) == LOW) {
+        task2Reccurency += additionalReccurency;
+        if (task2Reccurency > 10000) {
+          task2Reccurency = 10000;
+        }
+        lcdPrint("Decrease ferq.");
+        systemHasChanges = true;
+      }
+      if (digitalRead(BUTTON_DEC) == LOW) {
+        task2Reccurency -= additionalReccurency;
+        if (task2Reccurency < 50) {
+          task2Reccurency = 50;
+        }
+        lcdPrint("Increase ferq.");
+        systemHasChanges = true;
+      }
+    }
+}
+
+void taskLedBlink() {
+  if(redLedState == HIGH){
+      if (digitalRead(LED_BLINKING) == LOW) {
+        digitalWrite(LED_BLINKING, HIGH);
+      }
+      else{
+        digitalWrite(LED_BLINKING, LOW);
+      }
+    }
+}
+
+void taskIdle() {
+    if(systemHasChanges)
+    {
+      char* ledStatus = (redLedState == LOW) ? "off" : "on";
+      int greenFreq = task2Reccurency;
+      printf("============\n");
+      printf("Red LED: %s | Green LED frequency: %d ms\n", ledStatus, greenFreq);
+
+        if (digitalRead(BUTTON_INC) == LOW) {
+            printf("Button INC pressed\n");
+        }
+        if (digitalRead(BUTTON_DEC) == LOW) {
+            printf("Button DEC pressed\n");
+        }
+        if (digitalRead(BUTTON_TASK1) == LOW) {
+            printf("Button TASK1 pressed\n");
+        }
+
+      systemHasChanges = false;
+    }
+    delay(10);
+}
+
+static FILE *StdioStream;
+
+static int PutChar(char ch, FILE *f)
+{
+    return Serial.write(ch);
+}
+
+static int GetChar(FILE *f)
+{
+    while (!Serial.available());
+    return Serial.read();
+}
+
+void StdioSerialInit(void)
+{
+    Serial.begin(9600);
+    StdioStream = fdevopen(PutChar, GetChar);
+    stdin  = StdioStream;
+    stdout = StdioStream;
+}
+
+void lcdPrint(char* str){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(str); 
+}
--- src/lab2.2/lab22.cpp
+++ src/lab2.2/lab22.cpp
@@ -1,134 +1,176 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 #include <Arduino_FreeRTOS.h>
+#include <semphr.h>
+#include <queue.h>
 #include <stdio.h>
 
-#define BUTTON_TASK1 9
-#define BUTTON_INC 10
-#define BUTTON_DEC 11
-
-#define LED_DEPENDENT 3
-#define LED_BLINKING 2
+#define BUTTON_PIN 9
+#define BUTTON_RESET 10
+#define LED_TASK1_PIN 3
+#define LED_TASK2_PIN 2
 
 LiquidCrystal lcd(13, 12, 7, 6, 5, 4);
 
-volatile int interval = 500;
-const byte additionalInterval = 50;
+SemaphoreHandle_t xButtonSemaphore;
+QueueHandle_t xDataQueue;
 
-volatile byte redLedState = LOW;
-volatile byte greenLedState = LOW;
+byte N = 0;
+bool ledTask1State = LOW;
 
-byte previousButtonState = HIGH;
-byte currentButtonState = HIGH;
+void taskButton(void *pvParameters);
+void taskSync(void *pvParameters);
+void taskAsync(void *pvParameters);
+void taskReset(void *pvParameters);
 
-boolean systemHasChanges = true;
+static FILE *StdioStream;
 
-void setup() {
+static int PutChar(char ch, FILE *f) 
+{ 
+  return Serial.write(ch); 
+}
+
+static int GetChar(FILE *f) 
+{ 
+  while (!Serial.available()); return Serial.read(); 
+}
+
+void StdioSerialInit(void) 
+{
+  Serial.begin(9600);
+  StdioStream = fdevopen(PutChar, GetChar);
+  stdin  = StdioStream;
+  stdout = StdioStream;
+}
+
+void setup() 
+{
   StdioSerialInit();
-  lcd.begin(16, 2);
-  lcd.print("System status:");
 
-  pinMode(BUTTON_TASK1, INPUT_PULLUP);
-  pinMode(BUTTON_INC, INPUT_PULLUP);
-  pinMode(BUTTON_DEC, INPUT_PULLUP);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_RESET, INPUT_PULLUP);
+  pinMode(LED_TASK1_PIN, OUTPUT);
+  pinMode(LED_TASK2_PIN, OUTPUT);
 
-  pinMode(LED_DEPENDENT, OUTPUT);
-  pinMode(LED_BLINKING, OUTPUT);
+  xButtonSemaphore = xSemaphoreCreateBinary();
+  xDataQueue = xQueueCreate(32, sizeof(byte));
 
-  xTaskCreate(taskLedPushButton, "LED Button", 128, NULL, 1, NULL);
-  xTaskCreate(taskIncDecPushButton, "Inc/Dec Button", 128, NULL, 1, NULL);
-  xTaskCreate(taskLedBlink, "LED Blink", 128, NULL, 1, NULL);
-  xTaskCreate(idle, "Idle", 128, NULL, 1, NULL);
+  if (xButtonSemaphore != NULL && xDataQueue != NULL) 
+  {
+    xTaskCreate(taskButton, "Task1", 128, NULL, 1, NULL);
+    xTaskCreate(taskSync,   "Task2", 256, NULL, 1, NULL);
+    xTaskCreate(taskAsync,  "Task3", 256, NULL, 1, NULL);
+    xTaskCreate(taskReset,  "ResetTask", 256, NULL, 1, NULL);
+    xTaskCreate(taskN,  "NDisplayTask", 256, NULL, 1, NULL);
+  }
 
   vTaskStartScheduler();
 }
 
-void loop() {
+void loop() 
+{
 
 }
 
-void taskLedPushButton(void *pvParameters) {
-  for (;;) {
-    previousButtonState = currentButtonState;
-    currentButtonState = digitalRead(BUTTON_TASK1);
-    if (currentButtonState == LOW && currentButtonState != previousButtonState) {
-      redLedState = !redLedState;
-      digitalWrite(LED_DEPENDENT, redLedState);
-      vTaskDelay(pdMS_TO_TICKS(100));
-    }
-    vTaskDelay(pdMS_TO_TICKS(50));
-  }
-}
+void taskButton(void *pvParameters) 
+{
+  const TickType_t xFrequency = pdMS_TO_TICKS(10);
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+  byte prevState = HIGH;
 
-void taskIncDecPushButton(void *pvParameters) {
-  for (;;) {
-    if(redLedState == HIGH){
-      if (digitalRead(BUTTON_INC) == LOW) {
-        interval += additionalInterval;
-        if (interval > 1000) {
-          interval = 1000;
-        }
-      }
-      if (digitalRead(BUTTON_DEC) == LOW) {
-        interval -= additionalInterval;
-        if (interval < 50) {
-          interval = 50;
-        }
-      }
-    }
-    vTaskDelay(pdMS_TO_TICKS(50));
-  }
-}
-
-void taskLedBlink(void *pvParameters) {
-  unsigned long previousMillis = 0;
-  for (;;) {
-    if(redLedState){
-      unsigned long currentMillis = millis();
-      if (currentMillis - previousMillis >= interval) {
-        previousMillis = currentMillis;
-        greenLedState = !greenLedState;
-        digitalWrite(LED_BLINKING, greenLedState ? HIGH : LOW);
-      }
-    }
-    else{
-      greenLedState = LOW;
-      digitalWrite(LED_BLINKING, greenLedState);
-    }
-    vTaskDelay(pdMS_TO_TICKS(50));
-  }
-}
-
-void idle(void *pvParameters) {
-  unsigned long previousMillis = 0;
-  for (;;) {
-    if(systemHasChanges)
+  for (;;) 
+  {
+    byte currentState = digitalRead(BUTTON_PIN);
+    if (currentState == LOW && prevState == HIGH) 
     {
-      char* ledStatus = (redLedState == LOW) ? "off" : "on";
-      printf("Red LED is %s.\n", ledStatus);
-      printf("Green LED frequency - .\n");
+      digitalWrite(LED_TASK1_PIN, HIGH);
+      ledTask1State = HIGH;
+      vTaskDelay(pdMS_TO_TICKS(1000));
+      digitalWrite(LED_TASK1_PIN, LOW);
+      ledTask1State = LOW;
+
+      xSemaphoreGive(xButtonSemaphore);
     }
-    vTaskDelay(pdMS_TO_TICKS(50));
+    prevState = currentState;
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
   }
 }
 
-static FILE *StdioStream;
-
-static int PutChar(char ch, FILE *f)
+void taskSync(void *pvParameters) 
 {
-    return Serial.write(ch);
+  for (;;) 
+  {
+    if (xSemaphoreTake(xButtonSemaphore, portMAX_DELAY) == pdTRUE) 
+    {
+      N++;
+      if(N >= 255)
+      {
+        N = 255;
+      }
+
+      for (byte i = 1; i <= N; i++) 
+      {
+        xQueueSendToBack(xDataQueue, &i, 0);
+        vTaskDelay(pdMS_TO_TICKS(50)); 
+      }
+
+      byte endMarker = 0;
+      xQueueSendToBack(xDataQueue, &endMarker, 0);
+
+      for (byte i = 0; i < N; i++) 
+      {
+        digitalWrite(LED_TASK2_PIN, HIGH);
+        vTaskDelay(pdMS_TO_TICKS(300));
+        digitalWrite(LED_TASK2_PIN, LOW);
+        vTaskDelay(pdMS_TO_TICKS(500));
+      }
+    }
+  }
 }
 
-static int GetChar(FILE *f)
+void taskAsync(void *pvParameters) 
 {
-    while (!Serial.available());
-    return Serial.read();
+  byte receivedByte = 0;
+
+  for (;;) 
+  {
+    if (xQueueReceive(xDataQueue, &receivedByte, pdMS_TO_TICKS(200)) == pdTRUE) 
+    {
+      if (receivedByte == 0) 
+      {
+        printf("\n");
+      } else {
+        printf("%d ", receivedByte);
+      }
+    } else {
+      vTaskDelay(pdMS_TO_TICKS(200));
+    }
+  }
 }
 
-void StdioSerialInit(void)
+void taskReset(void *pvParameters) 
 {
-    Serial.begin(9600);
-    StdioStream = fdevopen(PutChar, GetChar);
-    stdin  = StdioStream;
-    stdout = StdioStream;
+  for (;;) 
+  {
+    if (digitalRead(BUTTON_RESET) == LOW) 
+    {
+      N = 0;
+      vTaskDelay(pdMS_TO_TICKS(300));
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(20));
+  }
+}
+
+void taskN(void *pvParameters) 
+{
+  for (;;) 
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("N = ");         
+    lcd.print(N);
+    lcd.print("   "); 
+
+    vTaskDelay(pdMS_TO_TICKS(200));
+  }
 }
